ded51acc4f0f0c745372db782aecac1bb80162f4
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-04-26 22:15:34 +0200

    Update TestSpectra.py

--- testspectra/TestSpectra.py
+++ testspectra/TestSpectra.py
@@ -5,19 +5,18 @@ s=input("Input starting label: ")
 
 n,m,b,A,f=0,0,0,0,0 #some initialisation
 
-x=np.linspace(0,20000,80000) #frequency sweep
+x=np.arange(1000).reshape((1000, 1)) #frequency sweep
 
 def S(x): #define low/high pass curve
     P=(m*x-b)*(-1)**n
     return A/(1+np.exp(P))
 
 for _ in range(int(N)):
-    n,m,b,A,f=np.random.uniform(0,20000,5) #b is the cutoff freq
+    n,m,b,A,f=np.random.uniform(0,1000,5) #b is the cutoff freq
     n=np.floor(n) #determines low/high pass
-    f/=20000 #noise level
-    A/=100 #frequency difference
-    m/=100 #cutoff gradient
-    e=np.random.normal(size=x.shape) #noise
+    f/=1000 #noise level
+    A/=10 #frequency difference
+    m/=10 #cutoff gradient
+    e=np.random.normal(size=(1000,1)) #noise
     y=S(x)+f*e
-    y.reshape((x.shape[0],1))
-    np.savetxt('LogRegTest{}.csv'.format(_+int(s)),y,fmt='%0.4f', delimiter=',')
+    np.savetxt('LogRegTest{}.csv'.format(_+int(s)),np.append(x,y,axis=1),fmt='%0.4f', delimiter=',') #save
